1485bca94479d834300a57ddd525f98e362cf3e1
Codebygithub <nguyenphuthinh040507@gmail.com>
2024-11-01 09:16:30 +0700

    Apikey and permissions Auth done

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,43 @@
+'use strict' ;
+
+const { findById } = require("../services/apiKey.service");
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION:'authorization',
+}
+const apiKey = async (req,res,next)=> {
+    try {
+        const key = req.headers[HEADER.API_KEY]?.toString();
+        if(!key) return res.status(403).json({
+            message:'Forbidden error'
+        })
+        const objKey = await findById(key)
+        if(!objKey) return res.status(403).json({
+            message:'Forbidden error'
+        })
+        req.objKey = objKey
+    } catch (error) {
+        next(error)
+    }
+}
+const permission = (permission) => {
+    return (req,res,next) => {
+        if(!req.objKey.permission) {
+            if(!objKey) return res.status(403).json({
+                message:'Permission denied'
+            })
+        }
+        const validPermission = req.objKey.permission.include(permission)
+        if(!validPermission) return res.status(403).json({
+            message:'Permission denied'
+
+        })
+        return next()
+
+    }
+}
+module.exports = {
+    apiKey,
+    permission
+}
